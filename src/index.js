dceba99efd4359eba7d41011af89e24c87590172
Dmitry <shcheklein.dev@gmail.com>
2026-01-25 20:16:46 +0300

    включение React.StrictMode

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //<React.StrictMode>
+  //<React.StrictMode> // Строгий режим лучше не выключать, но нужно учитывать что некоторые хуки могут срабатывать дважды в режиме разработки
     <App />
   //</React.StrictMode>
 );
