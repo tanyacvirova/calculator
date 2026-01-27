e4faf04e360192ca149e2d03202a4a9b97c10919
Dmitry <shcheklein.dev@gmail.com>
2026-01-27 10:56:44 +0300

    использование деструктуризации
    удобно использовать в компоненте, чтобы сократить цепочку и не обращаться к родителю, выглядит нагляднее)

--- src/components/PromoBlock.js
+++ src/components/PromoBlock.js
@@ -1,17 +1,17 @@
 import { promoCards } from "../constants/constants";
 
 function PromoBlock() {
-    const allCards = promoCards.map((card, i) => {
+    const allCards = promoCards.map(({tag, date, link, title, subtitle}, i) => {
         return (
             <div className="promo__card" key={i}>
                 <div className="card__tags">
                     <div className="card__tag">
-                        <p className="card__text">{card.tag}</p>
+                        <p className="card__text">{tag}</p>
                     </div>
-                    <p className="card__text">{card.date}</p>
+                    <p className="card__text">{date}</p>
                 </div>
-                <a className="card__link" href={card.link} target='_blanc' rel="noopener noreferrer">
-                    <h3 className="card__title">{card.title}<span className="extra"> {card.subtitle}</span></h3>
+                <a className="card__link" href={link} target='_blank' rel="noopener noreferrer">
+                    <h3 className="card__title">{title}<span className="extra"> {subtitle}</span></h3>
                 </a>
             </div>
         );
